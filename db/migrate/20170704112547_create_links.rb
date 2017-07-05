5c1c2d9fd0b51223c0d6829227a93c8bb4c0f63e
William Green <kulu1kulu2@gmail.com>
2017-07-05 08:13:41 +0000

    init

---
+++ db/migrate/20170704112547_create_links.rb
@@ -0,0 +1,11 @@
+class CreateLinks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :links do |t|
+      t.string :url
+      t.string :title
+      t.string :username
+
+      t.timestamps
+    end
+  end
+end
